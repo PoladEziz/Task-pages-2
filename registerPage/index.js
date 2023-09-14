8eb7ce48ad372f6339fc863447b7d93db5e52f00
PoladEziz <dspolad26@gmail.com>
2023-09-14 15:38:16 +0400

    incomplated task

---
+++ registerPage/index.js
@@ -0,0 +1,10 @@
+let emailInput=document.querySelector('.email')
+let nameInput=document.querySelector('.name')
+let passwordInput=document.querySelector('.password')
+let userObject={
+    email:[],
+    name:[],
+    password:[],
+    addUser(){
+    },
+}
